12c29d5e33f18e323ff9fe76d44f051240795dea
KysilRV <105556432+KysilRV@users.noreply.github.com>
2024-04-20 11:57:07 +0300

    created function get_available_letters

--- hangman.c
+++ hangman.c
@@ -34,24 +34,24 @@ int get_word(char secret[]){
     return 0;
 }
 
-int is_word_guessed(const char secret[], const char letters_guessed[]) {
-    int findFlag = 0;
+// int is_word_guessed(const char secret[], const char letters_guessed[]) {
+//     int findFlag = 0;
 
-    for (int i = 0; i < strlen(secret); i++) {
-        findFlag = 0;
-        for (int j = 0; j < strlen(letters_guessed); j++) {
-            if (secret[i] == letters_guessed[j]) {
-                findFlag = 1;
-                break;
-            }
-        }
-        if (findFlag == 0) {
-            return 0;
-        }
-    }
+//     for (int i = 0; i < strlen(secret); i++) {
+//         findFlag = 0;
+//         for (int j = 0; j < strlen(letters_guessed); j++) {
+//             if (secret[i] == letters_guessed[j]) {
+//                 findFlag = 1;
+//                 break;
+//             }
+//         }
+//         if (findFlag == 0) {
+//             return 0;
+//         }
+//     }
 
-    return 1;
-}
+//     return 1;
+// }
 
 void get_guessed_word(const char secret[], const char letters_guessed[], char guessed_word[]) {
     for (int i = 0; i < strlen(secret); i++) {
@@ -63,5 +63,70 @@ void get_guessed_word(const char secret[], const char letters_guessed[], char gu
                 guessed_word[i] = '_';
             }
         }
+    }
+    guessed_word[strlen(secret)] = '\0';
+}
+
+void get_available_letters(const char letters_guessed[], char available_letters[]) {
+    char alph[40] = "abcdefghijklmnopqrstuvwxyz";
+    int alphLen = strlen(alph);
+    int findFlag = 0;
+    int alphIndex = 0;
+
+    for (int i = 0; i < alphLen; i++) {
+        findFlag = 0;
+        for (int j = 0; j < strlen(letters_guessed); j++) {
+            if (alph[i] == letters_guessed[j]) {
+                findFlag = 1;
+                break;
+            }
+        }
+        if (findFlag == 0) {
+            available_letters[alphIndex] = alph[i];
+            alphIndex++;
+        }
+        available_letters[alphIndex] = '\0';
+    }
+}
+
+void hangman(const char secret[]) {
+    int secretLen = strlen(secret);
+    int attempts = 8;
+    char available_letters[40];
+    char letters_guessed[40];
+    char tmpInput[20];
+    int letters_guessed_index = 0;
+    char quessed_word[40];
+
+
+    printf("Welcome to the game, Hangman!\n");
+    printf("I am thinking of a word that is %d letters long.\n", secretLen);
+    printf("------------------------\n");
+
+    while (attempts > 0) {
+        printf("You have %d guesses left.\n", attempts);
+        get_available_letters(letters_guessed, available_letters);
+        printf("Available letters: %s\n", available_letters);
+        printf("Please guess a letter: ");
+        fgets(tmpInput, 20, stdin);
+        if (strlen(tmpInput) - 1 == 1) {
+            /// is a symbol
+            if (tmpInput[0] >= 97 && tmpInput[0] <= 122) {
+                letters_guessed[letters_guessed_index] = tmpInput[0];
+                letters_guessed_index++;
+            } else if (tmpInput[0] >= 65 && tmpInput[0] <= 90) {
+                tmpInput[0] += 32;
+                letters_guessed[letters_guessed_index] = tmpInput[0];
+                letters_guessed_index++;
+            } else {
+                printf("Oops! '%c' You've already guessed that letter: ", tmpInput[0]);
+            }
+            get_guessed_word(secret, letters_guessed, quessed_word);
+            printf("%s\n", quessed_word);
+        } else {
+            /// this is a tring
+        }
+ 
+        printf("------------------------\n");
     }
 }
--- main.c
+++ main.c
@@ -11,14 +11,7 @@ int main() {
     get_word(secret);
     printf("%s", secret);
 
-    printf("%d\n", is_word_guessed("secret", "aeiou"));
-    printf("%d\n", is_word_guessed("hello", "aeihoul"));
-
-    char result[30];
-    get_guessed_word("container", "arpstxgoieyu", result);
-
-    printf("%s\n", result);
-
+    hangman(secret);
 
     return 0;
 }
